d5a33b1049c5b659688ced3a9817f0b48658a20e
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-03-07 16:51:39 +0200

    Add esc_url around the_permalink() function.

--- includes/functions.php
+++ includes/functions.php
@@ -24,9 +24,9 @@ function webshare() { ?>
 	<div id="webshare-wrapper">
 		<ul id="webshare" class="webshare webshare-links">
 			<li><?php _e( 'Share:', 'webshare' ); ?></li>
-			<li class="webshare-facebook"><a class="webshare-facebook" href="https://www.facebook.com/sharer.php?u=<?php echo the_permalink(); ?>&t=<?php urlencode( the_title() ); ?>" target="_blank" title="<?php _e( 'Facebook', 'webshare' ); ?>"><span class="screen-reader-text webshare-link webshare-link-facebook"><?php _e( 'Facebook', 'webshare' ); ?></span></a></li>
-			<li class="webshare-twitter"><a class="webshare-twitter" href="https://twitter.com/intent/tweet?url=<?php echo the_permalink(); ?>&text=<?php urlencode( the_title() ); echo $via_twitter; ?>" target="_blank" title="<?php _e( 'Twitter', 'webshare' ); ?>"><span class="screen-reader-text webshare-link webshare-link-twitter"><?php _e( 'Twitter', 'webshare' ); ?></span></a></li>
-			<li class="webshare-google"><a class="webshare-google" href="https://plus.google.com/share?url=<?php echo the_permalink(); ?>" target="_blank" title="<?php _e( 'Google+', 'webshare' ); ?>"><span class="screen-reader-text webshare-link webshare-link-google"><?php _e( 'Google+', 'webshare' ); ?></span></a></li>
+			<li class="webshare-facebook"><a class="webshare-facebook" href="https://www.facebook.com/sharer.php?u=<?php echo esc_url( the_permalink() ); ?>&t=<?php urlencode( the_title() ); ?>" target="_blank" title="<?php _e( 'Facebook', 'webshare' ); ?>"><span class="screen-reader-text webshare-link webshare-link-facebook"><?php _e( 'Facebook', 'webshare' ); ?></span></a></li>
+			<li class="webshare-twitter"><a class="webshare-twitter" href="https://twitter.com/intent/tweet?url=<?php echo esc_url( the_permalink() ); ?>&text=<?php urlencode( the_title() ); echo $via_twitter; ?>" target="_blank" title="<?php _e( 'Twitter', 'webshare' ); ?>"><span class="screen-reader-text webshare-link webshare-link-twitter"><?php _e( 'Twitter', 'webshare' ); ?></span></a></li>
+			<li class="webshare-google"><a class="webshare-google" href="https://plus.google.com/share?url=<?php echo esc_url( the_permalink() ); ?>" target="_blank" title="<?php _e( 'Google+', 'webshare' ); ?>"><span class="screen-reader-text webshare-link webshare-link-google"><?php _e( 'Google+', 'webshare' ); ?></span></a></li>
 		</ul>
 	</div>
 
