9b2f630640458ed950f2b5dfe2de963d5722c9e5
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-03-29 12:09:23 +0200

    Add webshare after post content if theme has not declare support for the plugin.

--- includes/functions.php
+++ includes/functions.php
@@ -10,28 +10,72 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * @access public
  * @var    void
  */
-function webshare() { ?>
+function webshare() {
 
-	<?php 
+	echo get_webshare();
+
+}
+
+/**
+ * Get social links in a variable.
+ *
+ * @since  1.1
+ * @access public
+ * @var    void
+ */
+function get_webshare() {
+ 
 	/* Check if there is twitter account added in profile page. */
 	if( $twitter = get_the_author_meta( 'webshare_twitter' ) ) {
 		$via_twitter = '&via=' . esc_attr( $twitter );
 	} else {
 		$via_twitter = '';
 	}
-	?>
-	
-	<div id="webshare-wrapper">
-		<ul id="webshare" class="webshare webshare-links">
-			<li><?php _e( 'Share:', 'webshare' ); ?></li>
-			<li class="webshare-facebook"><a class="webshare-facebook" href="https://www.facebook.com/sharer.php?u=<?php echo esc_url( the_permalink() ); ?>&t=<?php urlencode( the_title() ); ?>" target="_blank" title="<?php _e( 'Facebook', 'webshare' ); ?>"><span class="screen-reader-text webshare-link webshare-link-facebook"><?php _e( 'Facebook', 'webshare' ); ?></span></a></li>
-			<li class="webshare-twitter"><a class="webshare-twitter" href="https://twitter.com/intent/tweet?url=<?php echo esc_url( the_permalink() ); ?>&text=<?php urlencode( the_title() ); echo $via_twitter; ?>" target="_blank" title="<?php _e( 'Twitter', 'webshare' ); ?>"><span class="screen-reader-text webshare-link webshare-link-twitter"><?php _e( 'Twitter', 'webshare' ); ?></span></a></li>
-			<li class="webshare-google"><a class="webshare-google" href="https://plus.google.com/share?url=<?php echo esc_url( the_permalink() ); ?>" target="_blank" title="<?php _e( 'Google+', 'webshare' ); ?>"><span class="screen-reader-text webshare-link webshare-link-google"><?php _e( 'Google+', 'webshare' ); ?></span></a></li>
-		</ul>
-	</div>
 
-<?php
+	/* Get all sharing options in variable. */
+	
+	do_action( 'webshare_before_output' ); // Action before output.
+	
+	$webshare_output .= '<div id="webshare-wrapper">';
+	$webshare_output .= '<ul id="webshare" class="webshare webshare-links">';
+	
+	do_action( 'webshare_before_first_list_element' ); // Action before first li element.
+	
+	$webshare_output .= '<li>' . __( 'Share:', 'webshare' ) . '</li>';
+	$webshare_output .= '<li class="webshare-facebook"><a class="webshare-facebook" href="https://www.facebook.com/sharer.php?u=' . esc_url( get_permalink() ) . '&t=' . urlencode( get_the_title() ) . '" target="_blank" title="' . __( 'Facebook', 'webshare' ) . '"><span class="screen-reader-text webshare-link webshare-link-facebook">' . __( 'Facebook', 'webshare' ) . '</span></a></li>';
+	$webshare_output .= '<li class="webshare-twitter"><a class="webshare-twitter" href="https://twitter.com/intent/tweet?url=' . esc_url( get_permalink() ) . '&text=' . urlencode( get_the_title() ) . $via_twitter . '" target="_blank" title="' . __( 'Twitter', 'webshare' ) . '"><span class="screen-reader-text webshare-link webshare-link-twitter">' . __( 'Twitter', 'webshare' ) . '</span></a></li>';
+	$webshare_output .= '<li class="webshare-google"><a class="webshare-google" href="https://plus.google.com/share?url=' . esc_url( get_permalink() ) . '" target="_blank" title="' . __( 'Google+', 'webshare' ) . '"><span class="screen-reader-text webshare-link webshare-link-google">' . __( 'Google+', 'webshare' ) . '</span></a></li>';
+	
+	do_action( 'webshare_after_last_list_element' ); // Action after last li element.
+	
+	$webshare_output .= '</ul>';
+	$webshare_output .= '</div>';
+	
+	do_action( 'webshare_after_output' ); // Action after output.
+	
+	/* Return output. */
+	return $webshare_output;
+	
 }
+
+/**
+ * Add webshare after content.
+ *
+ * @since  1.1
+ * @access public
+ * @var    void
+ */
+function webshare_add_sharing( $content ) {
+	
+	/* Check that we are not supporting webshare in our theme. And on singular 'post' and in main query. */
+	if( !current_theme_supports( 'webshare' ) && is_singular( apply_filters( 'webshare_when_to_show', array( 'post' ) ) ) && is_main_query() ) {
+		$webshare_add_social_sharing = sprintf( '%s', get_webshare() );
+		$content .= $webshare_add_social_sharing;	
+	}
+	
+	return $content;
+}
+add_filter('the_content', 'webshare_add_sharing' );
 
 /**
  * Add Twitter username in profile page.
