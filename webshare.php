59c90fbaca57a3e5f9c9c5a32085931e889c3638
Sami Keijonen <sami.keijonen@foxnet.fi>
2014-09-29 15:58:06 +0300

    Fix genericon path, genericon folder was missing.

--- webshare.php
+++ webshare.php
@@ -3,7 +3,7 @@
 * Plugin Name: WebShare
 * Plugin URI: https://foxland.fi/downloads/webshare
 * Description: Adds social sharing links.
-* Version: 1.2.3
+* Version: 1.2.4
 * Author: Sami Keijonen
 * Author URI: https://foxland.fi
 * Text Domain: webshare
@@ -17,7 +17,7 @@
 * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 *
 * @package WebShare
-* @version 1.2.3
+* @version 1.2.4
 * @author Sami Keijonen <sami.keijonen@foxnet.fi>
 * @copyright Copyright (c) 2014, Sami Keijonen
 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -90,7 +90,7 @@ final class WEBSHARE {
 		
 		/* Define Plugin Version. */
 		if ( ! defined( 'WEBSHARE_VERSION' ) ) {
-			define( 'WEBSHARE_VERSION', '1.2.3' );
+			define( 'WEBSHARE_VERSION', '1.2.4' );
 		}
 
 	}
@@ -159,7 +159,7 @@ final class WEBSHARE {
 		/* Enqueue the Genericons styles. */
 		wp_enqueue_style(
 			'genericons',
-			trailingslashit( plugin_dir_url( __FILE__ ) ) . "css/genericons/genericons$suffix.css",
+			trailingslashit( plugin_dir_url( __FILE__ ) ) . "css/genericons/genericons/genericons$suffix.css",
 			null,
 			'3.1'
 		);
